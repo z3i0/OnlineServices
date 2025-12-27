5a259ba6d3e6c363232a95911d02ff0b919aff00
Z3AMA <zedohany@outlook.com>
2025-12-27 22:21:02 +0200

    feat: add Footer and update HelloWorld layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,17 @@
 <script setup lang='ts'>
 import NavBar from "./ui/NavBar.vue"
+import Footer from "./ui/Footer.vue"
+defineProps<{
+  msg: string
+}>()
 </script>
 
 <template>
   <NavBar />
+  <main class="flex justify-center p-10">
+    <p class="text-3xl font-bold underline">
+      {{ msg }}
+    </p>
+  </main>
+  <Footer />
 </template>
